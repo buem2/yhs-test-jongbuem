2592d2be51759d39aa43f384ec19d09ddce25f94
buem2 <buem0306@naver.com>
2020-04-22 21:40:08 +0900

    2020-04-22 수정

--- sample.vue
+++ sample.vue
@@ -1,42 +1,43 @@
 <!-- HTML 시작 !-->
 <div id="app">
 	<v-app id="inspire">
-	    <div>
+		<div>
 			<v-row no-gutters>
 				<v-card color="black" width="100%">
-				  <div style='text-align:center'>
-					<span class="time">2020-03-14</span>
-				  </div>
+					<div style='text-align:center'>
+						<span class="time">2020-03-14 12:00:00</span>
+					</div>
 				</v-card>
-				<v-responsive width="100%"></v-responsive>
 				<template v-for="item in machine">
 					<v-col :key="item.idx">
-					  <v-card>
-						<v-row>
-						  <v-toolbar dense flat color="#b5b5b5" height="120">
-							<span class="f_mname">{{item.mid}}</span>
-							  <v-spacer></v-spacer>
-							<span class="f_p">{{item.process}}</span>
-						  </v-toolbar>
-						</v-row>
+						<v-card>
+							<v-row>
+								<v-toolbar dense flat color="#b5b5b5" height="120">
+									<span class="f_mname">{{item.mid}}</span>
+									<v-spacer></v-spacer>
+									<span class="f_p">{{item.process}}</span>
+								</v-toolbar>
+							</v-row>
 							<div class="text-center">
-							  <span class="f_end" style="align:center">{{item.timer}}</span>
+								<div class="f_end">
+									{{item.timer}}
+								</div>
 							</div>
 							<v-footer :fixed="true" height="100" absolute class="white">
-							  <v-row>
-								<v-progress-linear value="15" height="100"
-							  color="#357d21" background-color="#828282" rounded>
-								  <v-flex style="maxWidth:20px"></v-flex>
-								  <span class="vt">{{ item.count}}</span>
-								  <v-spacer></v-spacer>
-								  <span class="vt">
-									<div class="text-right">{{ item.fTime }}</div>
-								  </span>
-								  <v-flex style="maxWidth:20px"></v-flex>
-								</v-progress-linear>
-							  </v-row>
+								<v-row>
+									<v-progress-linear value="15" height="94"
+													   color="#357d21" background-color="#828282" rounded>
+										<v-flex style="maxWidth:20px"></v-flex>
+										<span class="vt">{{ item.count}}</span>
+										<v-spacer></v-spacer>
+										<span class="vt">
+											<div class="text-right">{{ item.fTime }}</div>
+										</span>
+										<v-flex style="maxWidth:20px"></v-flex>
+									</v-progress-linear>
+								</v-row>
 							</v-footer>
-					  </v-card>
+						</v-card>
 					</v-col>
 				</template>
 			</v-row>
@@ -47,6 +48,51 @@
 
 <style>
 <!-- CSS 시작 !-->
+body {
+	height: 100%;
+}
+.no-gutters {
+	height: 100%;
+}
+.v-card.v-sheet.theme--light.black {
+	height: 10%;
+}
+.v-card.v-sheet.theme--light.black>div {
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.v-application--wrap>div {
+	height: 100%;
+}
+.no-gutters>.col .v-card.v-sheet.theme--light {
+	height: 100%;
+}
+.col .v-card.v-sheet.theme--light .row {
+	height: 30%;
+}
+.col .v-card.v-sheet.theme--light .row .v-toolbar__content {
+	height: 100% !important;
+}
+.v-application .text-center {
+	height: 20%;
+}
+.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute {
+	height: 50% !important;
+}
+.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute .row {
+	height: 100%;
+}
+.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute .row .v-progress-linear.v-progress-linear--rounded.theme--light {
+	height: 100% !important;
+}
+.v-responsive__content {
+	height: 0;
+}
+.v-responsive {
+	height: 0;
+}
 .time {
 	font-size: 2.5vw;
 	color: #f4fc03;
@@ -66,43 +112,36 @@
 }
 .f_end {
 	font-size: 2vw;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
-.v-sheet.v-sheet--tile.theme--light.v-toolbar.v-toolbar--dense.v-toolbar--flat {
-	height: 120px !important;
-}
-
-.v-toolbar__content {
-	height: 120px !important;
-}
-
 .v-footer--absolute {
-	position: static;
-}
-.v-application .text-center {
-	height: 90px;
-	line-height: 90px;
+	position: relative;
 }
 .v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute {
 	width: 100%;
 	margin: 0;
 	padding: 0;
-	height: 140px !important;
-}
-.v-progress-linear.v-progress-linear--rounded.theme--light {
-	height: 140px !important;
 }
 .v-card.v-sheet.theme--light .row {
 	margin:0;
 }
-
 .no-gutters>.col {
 	flex: 0 33%;
-	margin-bottom: 0.5%;
+	height: 44%;
+	min-height: 44%;
 }
-.no-gutters .col:nth-child(3n+1) {
+.no-gutters .col:nth-child(3n) {
 	margin:0 0.5%;
 }
-
+.v-toolbar--dense .v-toolbar__content, .v-toolbar--dense .v-toolbar__extension {
+	height: 100% !important;
+}
+.theme--light.v-toolbar.v-sheet {
+	height: 100% !important;
+}
 .no-gutters > div:nth-child(4) .v-progress-linear__determinate {
 	background-color: #FF0000 !important;
 }
@@ -118,105 +157,66 @@
 .no-gutters > div:nth-child(8) .v-progress-linear__determinate {
 	background-color: #FF5E00 !important;
 }
-
-
-
 /* PC */
 @media all and ( min-width:1024px ) and ( max-width:1920px ) {
-	.v-sheet.v-sheet--tile.theme--light.v-toolbar.v-toolbar--dense.v-toolbar--flat {
-		height: 85px !important;
+	body {
+		font-size: 18px;
 	}
-	.v-toolbar__content {
-		height: 85px !important;
-	}
-	.v-application .text-center {
-		height: 60px;
-		line-height: 60px;
-	}
-	.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute {
-		height: 100px !important;
-	}
-	.v-progress-linear.v-progress-linear--rounded.theme--light {
-		height: 100px !important;
-	}
-
 }
-
-
-
 /* Tablet */
 @media all and ( min-width:768px ) and ( max-width:1023px ) {
-	.v-sheet.v-sheet--tile.theme--light.v-toolbar.v-toolbar--dense.v-toolbar--flat {
-		height: 70px !important;
+	body {
+		font-size: 13px;
 	}
-
 	.v-toolbar__content {
-		height: 70px !important;
+		padding: 4px 12px;
 	}
-	.v-application .text-center {
-		height: 50px;
-		line-height: 50px;
+	.time {
+		font-size: 1.5em;
+		color: #f4fc03;
+		text-align: center;
 	}
-	.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute {
-		height: 100px !important;
+	.f_mname {
+		font-size: 1.5em;
+		color: #333;
 	}
-	.v-progress-linear.v-progress-linear--rounded.theme--light {
-		height: 100px !important;
+	.f_p {
+		font-size: 1em;
+		color: #333;
 	}
-
+	.vt {
+		font-size: 1.3em;
+		color: #ffffff;
+	}
+	.f_end {
+		font-size: 1.2em;
+	}
 }
-
-
-
-/* mobile 2 */
+/* mobile */
 @media all and ( max-width:767px ) {
 	.no-gutters>.col {
 		flex: auto;
-		margin-bottom: 10px !important;
+		margin-bottom: 1% !important;
 	}
 	.time {
-		font-size: 2.5vw;
+		font-size: 1.5em;
+		color: #f4fc03;
+		text-align: center;
 	}
 	.f_mname {
-		font-size: 5vw;
+		font-size: 1.5em;
+		color: #333;
 	}
 	.f_p {
-		font-size: 4vw;
+		font-size: 1em;
+		color: #333;
 	}
 	.vt {
-		font-size: 4.5vw;
+		font-size: 1.3em;
+		color: #ffffff;
 	}
 	.f_end {
-		font-size: 3.5vw;
-	}
-	.v-sheet.v-sheet--tile.theme--light.v-toolbar.v-toolbar--dense.v-toolbar--flat {
-		height: 60px !important;
-	}
-
-	.v-toolbar__content {
-		height: 60px !important;
-	}
-	.v-application .text-center {
-		height: 40px;
-		line-height: 40px;
-	}
-	.v-footer.white.v-sheet.v-sheet--tile.theme--light.v-footer--absolute {
-		height: 80px !important;
-	}
-	.v-progress-linear.v-progress-linear--rounded.theme--light {
-		height: 80px !important;
-	}
-}
-
-
-/* mobile 1 */
-@media all and ( max-width:480px ) {
-	.f_mname {
-		font-size: 6vw;
-	}
-	.v-application .text-center {
-		height: 30px;
-		line-height: 30px;
+		font-size: 1.2em;
 	}
 }
 <!-- CSS 끝 !-->
